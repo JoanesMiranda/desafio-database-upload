6c73d36073e00b10ea1fadf78e13a745d19dd08c
Joanes Souto <joanes.souto@gmail.com>
2020-07-05 00:45:19 -0300

    finalizado o desafio 6

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,58 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number,
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Yout do not have enougth balance');
+    }
+
+    //verificar se a categoria existe
+    //Existe ? buscar ela no banco de dados e usar o id que foi retornado
+    //não existe? cria a categoria
+
+    const categoryRespository = getRepository(Category);
+
+    let transactionCategory = await categoryRespository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRespository.create({
+        title: category,
+      });
+      await categoryRespository.save(transactionCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
